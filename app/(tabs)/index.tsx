22b97d03b3e394f4295e4012006ed52e8322f90a
roobeen-b <rubinbaidhya@gmail.com>
2025-09-04 22:25:29 +0545

    Added the necessary assets and made changes to nav

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { Text, View } from "react-native";
 
-export default function Index() {
+const index = () => {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-2xl font-bold text-primary">
@@ -8,4 +9,6 @@ export default function Index() {
       </Text>
     </View>
   );
-}
+};
+
+export default index;
