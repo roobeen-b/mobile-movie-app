f497ee92381eb4332324be3e550760313e3fea41
roobeen-b <rubinbaidhya@gmail.com>
2025-09-09 23:27:29 +0545

    Implemented movie detail screen

--- services/api.ts
+++ services/api.ts
@@ -27,3 +27,26 @@ export const fetchMovies = async ({
   const data = await response.json();
   return data.results;
 };
+
+export const fetchMovieDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      }
+    );
+    if (!response.ok) {
+      // @ts-ignore
+      throw new Error("Failed to fetch movie details", response.statusText);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
